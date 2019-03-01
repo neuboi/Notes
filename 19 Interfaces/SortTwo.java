961d7d6793c9e31e13ed28ad30b6f574a204a0de
Neuboi <sadjei1@lsu.edu>
2019-03-01 14:58:21 -0600

    Should Change? 3/1

--- 19 Interfaces/SortTwo.java
+++ 19 Interfaces/SortTwo.java
@@ -8,26 +8,44 @@ import java.util.Arrays;
 
 public class SortTwo
 {
-	public static void main ( String[] args )
-	{
-		Comparable[] list =  {3,8,7,6,5,4,9};
-		
-		Arrays.sort(list); //uses the compareTo() method of each object
-		
-		for(Comparable num : list)
-		{
-			out.println(num);
-		}
+ public static void main ( String[] args )
+ {
+  Comparable[] list =  {3,8,7,6,5,4,9};
+  
+  Arrays.sort(list); //uses the compareTo() method of each object
+  
+  for(Comparable num : list)
+  {
+   out.println(num);
+  }
 
-		out.println("\n\n");
+  out.println("\n\n");
 
-		list = new String[]{"a","x","e","w","q"};
-		
-		Arrays.sort(list);
-		
-		for(Comparable let : list)
-		{
-			out.println(let);
-		}
+  list = new String[]{"a","x","e","w","q"};
+  
+  Arrays.sort(list);
+  
+  for(Comparable let : list)
+  {
+   out.println(let);
+  }
+    out.println("\n\n");
+  
+  Word n = new Word("HDK");
+    Word d = new Word("Thing");
+      Word s = new Word("Two");
+        Word g = new Word("You");
+          Word h = new Word("JJJ");
+            Word j = new Word("Io");
+              Word k = new Word("sad");
+  
+  list = new Word[]{n, d, s, g, h, j, k};
+  
+  Arrays.sort(list);
+  
+  for(Comparable let : list)
+  {
+   out.println(let);
+  }
   }
 }
